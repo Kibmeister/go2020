05c67ee5bffc8d3892255316f75bd4158c64f79f
Frederik Bay Nørgaard <201800246@post.au.dk>
2020-09-25 14:07:15 +0200

    .

--- assignment3/RSA.go
+++ assignment3/RSA.go
@@ -2,18 +2,51 @@ package main
 
 import ( "fmt" ; "math/big" ; "crypto/rand" )
 
-func keyGen(k int){
-	p, _ := rand.Prime(rand.Reader, k/2)
-	q, _ := rand.Prime(rand.Reader, k/2)
+var e = big.NewInt(3)
 
-	s := new(big.Int)
-	fmt.Println(p)
-	fmt.Println(q)
-	
-	var n = s.Mul(p,q)
-	fmt.Println(n)
+type PublicKey struct {
+	PublicKeyE *big.Int
+	PublicKeyN *big.Int
 }
 
+func makePublicKey() *PublicKey {
+	PublicKey := new(PublicKey)
+	return PublicKey
+}
+
+func keyGen(k int) *PublicKey{
+	PublicKey := makePublicKey()
+
+	p, _ := rand.Prime(rand.Reader, k/2)
+	q, _ := rand.Prime(rand.Reader, k - k/2)
+	fmt.Println(p)
+	fmt.Println(q)
+
+	n := new(big.Int)
+	
+	n = n.Mul(p,q)
+	PublicKey.PublicKeyN = n
+	
+	
+	PublicKey.PublicKeyE = e
+
+	one := big.NewInt(1)
+
+	sum := new(big.Int)
+
+	sum = sum.Mul(sum.Sub(p, one), sum.Sub(q, one))
+
+	d := new(big.Int)
+
+	d = d.ModInverse(e, sum)
+
+	fmt.Println("privatekey", d)
+	fmt.Println("publickey", PublicKey.PublicKeyN, PublicKey.PublicKeyE)
+
+	return PublicKey
+}
+
+
 func main() {
-	keyGen(124)
+	keyGen(4)
 }
