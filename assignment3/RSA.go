dc1ecdd3fe0e62e702faac215809aafc09ae72ab
Kasper Iverslien Borgbjerg <201808262@post.au.dk>
2020-09-28 13:45:22 +0200

    check for gcd

--- assignment3/RSA.go
+++ assignment3/RSA.go
@@ -16,37 +16,84 @@ func makePublicKey() *PublicKey {
 
 func keyGen(k int) *PublicKey{
 	PublicKey := makePublicKey()
+	p, _ := rand.Prime(rand.Reader, k/2) // makes a random prime 
+	q, _ := rand.Prime(rand.Reader, k - k/2)  // makes a random prime 
+	fmt.Println("the first random prime : ", p)
+	fmt.Println("the second random prime : ", q)
 
-	p, _ := rand.Prime(rand.Reader, k/2)
-	q, _ := rand.Prime(rand.Reader, k - k/2)
-	fmt.Println(p)
-	fmt.Println(q)
-
-	n := new(big.Int)
+	n := new(big.Int) 
 	
-	n = n.Mul(p,q)
+	n = n.Mul(p,q) // first digit in our public key  
 	PublicKey.PublicKeyN = n
 	
-	
+	e := big.NewInt(3)// the exponent to match with the public key 
 	PublicKey.PublicKeyE = e
 
 	one := big.NewInt(1)
 
-	sum := new(big.Int)
+	primesSubtract := new(big.Int)
 
-	sum = sum.Mul(sum.Sub(p, one), sum.Sub(q, one))
+	primesSubtract = primesSubtract.Mul(primesSubtract.Sub(p, one), primesSubtract.Sub(q, one))
 
-	d := new(big.Int)
+	gcdP := new(big.Int).GCD(nil, nil, e, p.Sub(p, one)) // the greates common divisor for p
+	gcdQ := new(big.Int).GCD(nil, nil, e, q.Sub(q, one)) // the greates common divisor for q
 
-	d = d.ModInverse(e, sum)
+	if gcdP == big.NewInt(1) && gcdQ == big.NewInt(1)  {
+		d := e.ModInverse(e, primesSubtract)
+		fmt.Println("this is the privateKey :", d)
+	}
 
-	fmt.Println("privatekey", d)
-	fmt.Println("publickey", PublicKey.PublicKeyN, PublicKey.PublicKeyE)
+	//fmt.Println("privatekey", d)
+	fmt.Println("this is the publicKey", PublicKey.PublicKeyN, PublicKey.PublicKeyE)
 
 	return PublicKey
 }
 
-
 func main() {
-	keyGen(4)
+	keyGen(106)
 }
+
+
+
+// type keys struct {
+// 	privatK *big.Int 
+// 	publicK *big.Int 
+// }
+
+// func keyGen(k int){
+// 	p, _ := rand.Prime(rand.Reader, k/2)
+// 	q, _ := rand.Prime(rand.Reader, k - k/2)
+
+// 	s:= new(big.Int)
+// 	one:= big.NewInt(1)
+// 	//fmt.Println("this is the input k :", k)
+
+// 	e := big.NewInt(3)
+// 	// generate a modulus og length |k|
+
+// 	// this is the public key
+// 	n:= s.Mul(p,q)
+ 
+
+// 	sum := s.Mul(s.Sub(p, one), s.Sub(q, one))
+
+// 	gcdP := new(big.Int).GCD(nil, nil, e, p.Sub(p, one)) // the greates common divisor for P
+// 	gcdQ := new(big.Int).GCD(nil, nil, e, q.Sub(q, one)) // the greates common divisor for Q
+
+// 	if gcdP == big.NewInt(1) && gcdQ == big.NewInt(1)  {
+// 		fmt.Println("the conditions for the private key is fulfillied")
+// 		d := e.ModInverse(e, sum)
+// 		fmt.Println("this is my privateKey :", d)
+// 	}
+
+
+// 	// this is the private key
+		
+// 	fmt.Println("this is my publicKey  :", n)
+// }
+
+// func main() {
+// 	keyGen(50)
+// }
+
+
